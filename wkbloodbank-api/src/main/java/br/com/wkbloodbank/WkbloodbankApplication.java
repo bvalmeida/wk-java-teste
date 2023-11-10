6f75fe1be6f4cb5d9fc487bda102137f6d0f209f
bvalmeida <bvalmeida94@gmail.com>
2023-11-10 19:52:08 -0300

    iniciando backend core, docker-compose com mysql confg e conexao

---
+++ wkbloodbank-api/src/main/java/br/com/wkbloodbank/WkbloodbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.wkbloodbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkbloodbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkbloodbankApplication.class, args);
+	}
+
+}
---
+++ wkbloodbank-api/src/test/java/br/com/wkbloodbank/WkbloodbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wkbloodbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkbloodbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
